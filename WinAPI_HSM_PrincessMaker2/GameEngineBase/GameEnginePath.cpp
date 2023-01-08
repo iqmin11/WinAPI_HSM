a0b3acc7cc7f3103fed01cc3d3c6d10bc3af3638
승민 황 <iqmin11@gmail.com>
2023-01-08 16:59:27 +0900

    황승민 : GameEnginePath 커밋

---
+++ WinAPI_HSM_PrincessMaker2/GameEngineBase/GameEnginePath.cpp
@@ -0,0 +1,89 @@
+#include "GameEnginePath.h"
+#include <Windows.h> // 왜필요하지?
+#include <GameEngineBase/GameEngineDebug.h>
+#include <io.h>
+
+GameEnginePath::GameEnginePath()
+	:Path(std::filesystem::current_path()) //exe파일이 실행된 위치
+{
+
+}
+
+GameEnginePath::GameEnginePath(std::filesystem::path _Path)
+	:Path(_Path) 
+{
+}
+
+GameEnginePath::GameEnginePath(const std::string& _Path)
+	:Path(_Path)
+{
+}
+
+GameEnginePath::~GameEnginePath()
+{
+
+}
+
+GameEnginePath::GameEnginePath(const GameEnginePath& _Other)
+	: Path(_Other.Path)
+{
+}
+
+GameEnginePath::GameEnginePath(GameEnginePath&& _Other)
+	: Path(_Other.Path)
+{
+}
+
+std::string GameEnginePath::GetPathToString() const
+{
+	return Path.string();
+}
+
+void GameEnginePath::MoveParent()
+{
+	Path = Path.parent_path();
+}
+
+void GameEnginePath::MoveParentToChildPath(const std::string_view& _String)
+{
+	while (false == IsRoot()) // 상위폴더가 있어?
+	{
+		if (true == IsExistsToPlusString(_String)) // 있으면 해당경로에 접근이 가능해? (지금 디렉토리에 해당 파일이 있어?)
+		{
+			return; //그 폴더가 있는 Path인채로 함수 종료
+		}
+
+		MoveParent(); // 상위폴더를 Path로
+	}
+
+	MsgAssert("이런 경로를 자식으로 가진 부모는 존재하지 않습니다."); // 상위폴더 없음.
+}
+
+bool GameEnginePath::IsRoot()
+{
+	return Path == Path.root_path(); // 패스가 루트 패스야?
+}
+
+bool GameEnginePath::IsExistsToPlusString(const std::string_view& _String) // 니가 인자로 받은 파일명을 현재 Path에서 접근 가능해??
+{
+	std::string Str = GetPathToString() + _String.data(); // Path + 파일명 → 해당파일 실행경로
+	return 0 == _access(Str.c_str(), 0); // 그 경로에 접근이 가능해? 가능해면 0 리턴 ,인자로 받는 0은 검색 수준임 자세한건 msdn 참고
+}
+
+bool GameEnginePath::IsExists()
+{
+	return 0 == _access(Path.string().c_str(), 0); 
+}
+
+bool GameEnginePath::Move(const std::string_view& _Path)
+{
+	Path += _Path;
+	if (false == IsExists())
+	{
+		MsgAssert("존재하지 않는 경로로 이동하려고 했습니다.");
+		return false;
+	}
+	return true;
+}
+
+
---
+++ WinAPI_HSM_PrincessMaker2/GameEngineBase/GameEnginePath.h
@@ -0,0 +1,37 @@
+#pragma once
+// c++이 실행되는 곳에서 사용할수 있는 모든 경로 관련 처리방식을 지원해준다.
+#include <filesystem> 
+
+class GameEnginePath // 경로에 관련한 기능을 대표하는 클래스
+{
+public:
+	// construtor destructor
+	GameEnginePath();
+	GameEnginePath(std::filesystem::path _Path);
+	GameEnginePath(const std::string& _Path);
+	~GameEnginePath();
+
+	// delete Function
+	GameEnginePath(const GameEnginePath& _Other); // 쉽게말하면 L-Value를 인자로 받는 복사생성자
+	GameEnginePath(GameEnginePath&& _Other); // 쉽게말하면 R-Value를 인자로 받는 이동생성자
+	GameEnginePath& operator=(const GameEnginePath& _Other) = delete;
+	GameEnginePath& operator=(GameEnginePath&& _Other) noexcept = delete;
+
+	std::string GetPathToString() const;
+	void MoveParent(); //Path를 상위폴더Path로 바꿈
+	void MoveParentToChildPath(const std::string_view& _String); // 인자의 파일명이 있을때까지 상위폴더로 이동 리소스 폴더는 상위폴더에 만들거기 떄문에 이렇게 구성했나봐
+	bool IsRoot(); // 패스가 루트 패스야?
+	bool IsExistsToPlusString(const std::string_view& _String); // 니가 인자로 받은 파일명을 현재 Path에서 접근 가능해??
+	bool IsExists(); // 그 경로가 실제로 있어?
+	bool Move(const std::string_view& _Path); // 파일명을 받으면 현재 경로랑 합쳐서 파일 실행 경로로 이동
+
+
+protected:
+
+
+private:
+
+	std::filesystem::path Path;
+
+};
+
