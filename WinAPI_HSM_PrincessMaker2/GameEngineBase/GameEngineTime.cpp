2cbff2ed71971d47bb519f7f0036e9938e588c23
승민 황 <iqmin11@gmail.com>
2023-01-13 11:10:58 +0900

    황승민 : GameEngineTime 커밋

--- WinAPI_HSM_PrincessMaker2/GameEngineBase/GameEngineTime.cpp
+++ WinAPI_HSM_PrincessMaker2/GameEngineBase/GameEngineTime.cpp
@@ -2,7 +2,8 @@
 
 GameEngineTime::GameEngineTime()
 {
-	QueryPerformanceFrequency(&Second);
+	QueryPerformanceFrequency(&Second); // 이 컴퓨터는 1초를 정수n이라는 숫자로 표현할꺼야
+	QueryPerformanceCounter(&Prev); // 이제부터 시간 체크
 }
 
 GameEngineTime::~GameEngineTime()
@@ -10,13 +11,23 @@ GameEngineTime::~GameEngineTime()
 
 }
 
-void GameEngineTime::TimeCheckStart()
+void GameEngineTime::Reset()
 {
-	QueryPerformanceCounter(&Prev);
+	QueryPerformanceCounter(&Prev); // 측정 시작시간 현재로 변경
 }
 
-float GameEngineTime::TimeCheckEnd()
+float GameEngineTime::TimeCheck()
 {
+	QueryPerformanceCounter(&Current);
+	
+	Tick = Current.QuadPart - Prev.QuadPart;
 
-	return 0.0f;
+	DoubleDeltaTime = (static_cast<double>(Current.QuadPart) - static_cast<double>(Prev.QuadPart)) / static_cast<double>(Second.QuadPart); // 걸린시간을 초로 나타내는것
+
+	Prev.QuadPart = Current.QuadPart; // 타임체크가 끝나면 다시 타임을 체크할 준비(시간 초기화)
+
+	floatDeltaTime = static_cast<float>(DoubleDeltaTime); // 단순 형변환(플로트를 많이 쓰기 때문에 이것으로 형변환)
+
+	return floatDeltaTime;
 }
+
--- WinAPI_HSM_PrincessMaker2/GameEngineBase/GameEngineTime.h
+++ WinAPI_HSM_PrincessMaker2/GameEngineBase/GameEngineTime.h
@@ -15,21 +15,21 @@ public:
 	GameEngineTime& operator=(const GameEngineTime& _Other) = delete;
 	GameEngineTime& operator=(GameEngineTime&& _Other) noexcept = delete;
 
-	void TimeCheckStart();
+	float TimeCheck();
 
 	// 이 사이에 벌어진 일의 시간을 재줘
 
-	float TimeCheckEnd();
 
 
 protected:
 
 
 private:
+	__int64 Tick = 0;
 	LARGE_INTEGER Prev = LARGE_INTEGER();
 	LARGE_INTEGER Current = LARGE_INTEGER();
 	LARGE_INTEGER Second = LARGE_INTEGER();
-
-
+	double DoubleDeltaTime = 0.0;
+	float floatDeltaTime = 0.0;
 };
 
