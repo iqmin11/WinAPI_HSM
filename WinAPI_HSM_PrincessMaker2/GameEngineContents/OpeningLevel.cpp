386d1e67fa402960535816e174476ff02467cd99
승민 황 <iqmin11@gmail.com>
2023-01-28 22:55:30 +0900

    황승민 : 오프닝레벨 리소스 추가 커밋

--- WinAPI_HSM_PrincessMaker2/GameEngineContents/OpeningLevel.cpp
+++ WinAPI_HSM_PrincessMaker2/GameEngineContents/OpeningLevel.cpp
@@ -1,5 +1,6 @@
 #include "OpeningLevel.h"
-
+#include <GameEngineBase/GameEngineDirectory.h>
+#include <GameEngineCore/GameEngineResources.h>
 
 OpeningLevel::OpeningLevel()
 {
@@ -13,6 +14,48 @@ OpeningLevel::~OpeningLevel()
 
 void OpeningLevel::Loading()
 {
+	GameEngineDirectory Dir;
+
+	Dir.MoveParentToDirectory("ContentsResources");
+	Dir.Move("ContentsResources");
+	{
+		GameEngineImage* Image = GameEngineResources::GetInst().ImageLoad(Dir.GetPlusFileName("FrameSample1.BMP"));
+		Image->Cut(3, 3);
+	}
+
+	{
+		GameEngineImage* Image = GameEngineResources::GetInst().ImageLoad(Dir.GetPlusFileName("FrameSample2.BMP"));
+		Image->Cut(3, 3);
+	}
+
+	{
+		GameEngineImage* Image = GameEngineResources::GetInst().ImageLoad(Dir.GetPlusFileName("FrameSample3.BMP"));
+		Image->Cut(3, 3);
+	}
+
+	GameEngineResources::GetInst().ImageLoad(Dir.GetPlusFileName("MugShotFrame_God.BMP"));
+	GameEngineResources::GetInst().ImageLoad(Dir.GetPlusFileName("MugShotFrame_Nomal.BMP"));
+
+
+	Dir.Move("OpeningLevel");
+	{
+		GameEngineImage* Image = GameEngineResources::GetInst().ImageLoad(Dir.GetPlusFileName("DayNum.BMP"));
+		Image->Cut(20, 1);
+	}
+	{
+		GameEngineImage* Image = GameEngineResources::GetInst().ImageLoad(Dir.GetPlusFileName("MonthNum.BMP"));
+		Image->Cut(10, 1);
+	}
+	GameEngineResources::GetInst().ImageLoad(Dir.GetPlusFileName("Credit.BMP"));
+	GameEngineResources::GetInst().ImageLoad(Dir.GetPlusFileName("Cutscene1.BMP"));
+	GameEngineResources::GetInst().ImageLoad(Dir.GetPlusFileName("Cutscene2.BMP"));
+	GameEngineResources::GetInst().ImageLoad(Dir.GetPlusFileName("Cutscene3.BMP"));
+	GameEngineResources::GetInst().ImageLoad(Dir.GetPlusFileName("Cutscene4.BMP"));
+	GameEngineResources::GetInst().ImageLoad(Dir.GetPlusFileName("Cutscene5.BMP"));
+	GameEngineResources::GetInst().ImageLoad(Dir.GetPlusFileName("Cutscene6.BMP"));
+	GameEngineResources::GetInst().ImageLoad(Dir.GetPlusFileName("Cutscene7.BMP"));
+	GameEngineResources::GetInst().ImageLoad(Dir.GetPlusFileName("OpeningDate.BMP"));
+	GameEngineResources::GetInst().ImageLoad(Dir.GetPlusFileName("TitleLogo.BMP"));
 
 }
 
