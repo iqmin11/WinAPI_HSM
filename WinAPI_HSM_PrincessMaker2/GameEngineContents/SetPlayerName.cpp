f0d1592a1b0da56a06742e592fdbc779fafd50c4
승민 황 <iqmin11@gmail.com>
2023-02-15 17:06:36 +0900

    황승민 : SetPlayerName 수정

--- WinAPI_HSM_PrincessMaker2/GameEngineContents/SetPlayerName.cpp
+++ WinAPI_HSM_PrincessMaker2/GameEngineContents/SetPlayerName.cpp
@@ -12,7 +12,6 @@ std::string SetPlayerName::PrintLetter = "\0";
 
 SetPlayerName::SetPlayerName()
 {
-
 }
 
 SetPlayerName::~SetPlayerName()
@@ -48,64 +47,8 @@ void SetPlayerName::Start()
 	SetLetterEraseButton();
 	SetLetterEraseButtonRender();
 
-	// functional과 람다를 사용하는 방법이 있다.
-	UpperCaseButton[0][0]->SetClickCallBack(PushBack_Button);
-	UpperCaseButton[0][1]->SetClickCallBack(PushBack_Button);
-	UpperCaseButton[0][2]->SetClickCallBack(PushBack_Button);
-	UpperCaseButton[0][3]->SetClickCallBack(PushBack_Button);
-	UpperCaseButton[0][4]->SetClickCallBack(PushBack_Button);
-	UpperCaseButton[1][0]->SetClickCallBack(PushBack_Button);
-	UpperCaseButton[1][1]->SetClickCallBack(PushBack_Button);
-	UpperCaseButton[1][2]->SetClickCallBack(PushBack_Button);
-	UpperCaseButton[1][3]->SetClickCallBack(PushBack_Button);
-	UpperCaseButton[1][4]->SetClickCallBack(PushBack_Button);
-	UpperCaseButton[2][0]->SetClickCallBack(PushBack_Button);
-	UpperCaseButton[2][1]->SetClickCallBack(PushBack_Button);
-	UpperCaseButton[2][2]->SetClickCallBack(PushBack_Button);
-	UpperCaseButton[2][3]->SetClickCallBack(PushBack_Button);
-	UpperCaseButton[2][4]->SetClickCallBack(PushBack_Button);
-	UpperCaseButton[3][0]->SetClickCallBack(PushBack_Button);
-	UpperCaseButton[3][1]->SetClickCallBack(PushBack_Button);
-	UpperCaseButton[3][2]->SetClickCallBack(PushBack_Button);
-	UpperCaseButton[3][3]->SetClickCallBack(PushBack_Button);
-	UpperCaseButton[3][4]->SetClickCallBack(PushBack_Button);
-	UpperCaseButton[4][0]->SetClickCallBack(PushBack_Button);
-	UpperCaseButton[4][1]->SetClickCallBack(PushBack_Button);
-	UpperCaseButton[4][2]->SetClickCallBack(PushBack_Button);
-	UpperCaseButton[4][3]->SetClickCallBack(PushBack_Button);
-	UpperCaseButton[4][4]->SetClickCallBack(PushBack_Button);
-	UpperCaseButton[5][0]->SetClickCallBack(PushBack_Button);
-	
-	LowerCaseButton[0][0]->SetClickCallBack(PushBack_Button);
-	LowerCaseButton[0][1]->SetClickCallBack(PushBack_Button);
-	LowerCaseButton[0][2]->SetClickCallBack(PushBack_Button);
-	LowerCaseButton[0][3]->SetClickCallBack(PushBack_Button);
-	LowerCaseButton[0][4]->SetClickCallBack(PushBack_Button);
-	LowerCaseButton[1][0]->SetClickCallBack(PushBack_Button);
-	LowerCaseButton[1][1]->SetClickCallBack(PushBack_Button);
-	LowerCaseButton[1][2]->SetClickCallBack(PushBack_Button);
-	LowerCaseButton[1][3]->SetClickCallBack(PushBack_Button);
-	LowerCaseButton[1][4]->SetClickCallBack(PushBack_Button);
-	LowerCaseButton[2][0]->SetClickCallBack(PushBack_Button);
-	LowerCaseButton[2][1]->SetClickCallBack(PushBack_Button);
-	LowerCaseButton[2][2]->SetClickCallBack(PushBack_Button);
-	LowerCaseButton[2][3]->SetClickCallBack(PushBack_Button);
-	LowerCaseButton[2][4]->SetClickCallBack(PushBack_Button);
-	LowerCaseButton[3][0]->SetClickCallBack(PushBack_Button);
-	LowerCaseButton[3][1]->SetClickCallBack(PushBack_Button);
-	LowerCaseButton[3][2]->SetClickCallBack(PushBack_Button);
-	LowerCaseButton[3][3]->SetClickCallBack(PushBack_Button);
-	LowerCaseButton[3][4]->SetClickCallBack(PushBack_Button);
-	LowerCaseButton[4][0]->SetClickCallBack(PushBack_Button);
-	LowerCaseButton[4][1]->SetClickCallBack(PushBack_Button);
-	LowerCaseButton[4][2]->SetClickCallBack(PushBack_Button);
-	LowerCaseButton[4][3]->SetClickCallBack(PushBack_Button);
-	LowerCaseButton[4][4]->SetClickCallBack(PushBack_Button);
-	LowerCaseButton[5][0]->SetClickCallBack(PushBack_Button);
-
 	LetterEraseButton->SetClickCallBack(PopBack_PrintLetter);
 
-	//CompleteButton->SetClickCallBack(NextActor);
 	Off();
 }
 
@@ -218,9 +161,9 @@ void SetPlayerName::Update(float _DeltaTime)
 	}
 	
 	
-	PrintLetterRender1->SetText(" " + PrintLetter, LetterRenderHeight, TextType, TextAlign::Left, TextColor);
-	PrintLetterRender2->SetText(" " + PrintLetter, LetterRenderHeight, TextType, TextAlign::Left, TextColor);
-	PrintLetterShadow->SetText(" " + PrintLetter, LetterRenderHeight, TextType, TextAlign::Left, TextShadowColor);
+	PrintLetterRender1->SetText(" " + PrintLetter, LetterRenderHeight, TextType, TextAlign::Left, TextEffect1Color);
+	PrintLetterRender2->SetText(" " + PrintLetter, LetterRenderHeight, TextType, TextAlign::Left, TextEffect2Color);
+	PrintLetterRender3->SetText(" " + PrintLetter, LetterRenderHeight, TextType, TextAlign::Left, TextEffect3Color);
 }
 
 void SetPlayerName::Render(float _DeltaTime)
@@ -240,7 +183,6 @@ void SetPlayerName::On()
 			}
 			UpperCaseButton[y][x]->On();
 			LowerCaseButton[y][x]->On();
-			
 		}
 	}
 	CompleteButton->On();
@@ -271,9 +213,9 @@ void SetPlayerName::SetPrintLetterRender()
 	PrintLetterRender2->SetText(" " + PrintLetter, LetterRenderHeight, TextType, TextAlign::Left, TextColor);
 	PrintLetterRender2->SetPosition(SetPrintLetterRenderPos + float4::Up * float4{ 0,1 });
 
-	PrintLetterShadow = CreateRender(PM2RenderOrder::Menu0_Display_Shadow);
-	PrintLetterShadow->SetText(" " + PrintLetter, LetterRenderHeight, TextType, TextAlign::Left, TextShadowColor);
-	PrintLetterShadow->SetPosition(SetPrintLetterRenderPos + float4::Down * float4{ 0,1 });
+	PrintLetterRender3 = CreateRender(PM2RenderOrder::Menu0_Display_Shadow);
+	PrintLetterRender3->SetText(" " + PrintLetter, LetterRenderHeight, TextType, TextAlign::Left, TextShadowColor);
+	PrintLetterRender3->SetPosition(SetPrintLetterRenderPos + float4::Down * float4{ 0,1 });
 }
 
 void SetPlayerName::SetExplainRender()
@@ -313,6 +255,7 @@ void SetPlayerName::SetUpperCaseButton()
 			UpperCaseButton[y][x]->SetHoverImage("SelectButton_Release.Bmp");
 			UpperCaseButton[y][x]->SetPressImage("SelectButton_Release.Bmp");
 			UpperCaseButton[y][x]->Chracter = Start++;
+			UpperCaseButton[y][x]->SetClickCallBack(PushBack_Button);
 		}
 	}
 }
@@ -384,6 +327,7 @@ void SetPlayerName::SetLowerCaseButton()
 			LowerCaseButton[y][x]->SetHoverImage("SelectButton_Release.Bmp");
 			LowerCaseButton[y][x]->SetPressImage("SelectButton_Release.Bmp");
 			LowerCaseButton[y][x]->Chracter = Start++;
+			LowerCaseButton[y][x]->SetClickCallBack(PushBack_Button);
 		}
 	}
 }
@@ -525,270 +469,6 @@ void SetPlayerName::SetLetterEraseButtonRender()
 	LetterEraseButtonShadowRender = CreateRender(PM2RenderOrder::Menu0_Display_Shadow);
 	LetterEraseButtonShadowRender->SetText("뒤로", LetterRenderHeight, TextType, TextAlign::Center, TextShadowColor);
 	LetterEraseButtonShadowRender->SetPosition(ButtonStartPosition + float4::Down * float4{ 0,1 });
-}
-
-void SetPlayerName::PushBack_A()
-{
-	//if (4 < PrintLetter.size())
-	//{
-	//	return;
-	//}
-	PrintLetter.push_back('A');
-}
-
-void SetPlayerName::PushBack_B()
-{
-	PrintLetter.push_back('B');
-}
-
-void SetPlayerName::PushBack_C()
-{
-	PrintLetter.push_back('C');
-}
-
-void SetPlayerName::PushBack_D()
-{
-	PrintLetter.push_back('D');
-}
-
-void SetPlayerName::PushBack_E()
-{
-	PrintLetter.push_back('E');
-}
-
-void SetPlayerName::PushBack_F()
-{
-	PrintLetter.push_back('F');
-}
-
-void SetPlayerName::PushBack_G()
-{
-	PrintLetter.push_back('G');
-}
-
-void SetPlayerName::PushBack_H()
-{
-	PrintLetter.push_back('H');
-}
-
-void SetPlayerName::PushBack_I()
-{
-	PrintLetter.push_back('I');
-}
-
-void SetPlayerName::PushBack_J()
-{
-	PrintLetter.push_back('J');
-}
-
-void SetPlayerName::PushBack_K()
-{
-	PrintLetter.push_back('K');
-}
-
-void SetPlayerName::PushBack_L()
-{
-	PrintLetter.push_back('L');
-}
-
-void SetPlayerName::PushBack_M()
-{
-	PrintLetter.push_back('M');
-}
-
-void SetPlayerName::PushBack_N()
-{
-	PrintLetter.push_back('N');
-}
-
-void SetPlayerName::PushBack_O()
-{
-	PrintLetter.push_back('O');
-}
-
-void SetPlayerName::PushBack_P()
-{
-	PrintLetter.push_back('P');
-}
-
-void SetPlayerName::PushBack_Q()
-{
-	PrintLetter.push_back('Q');
-}
-
-void SetPlayerName::PushBack_R()
-{
-	PrintLetter.push_back('R');
-}
-
-void SetPlayerName::PushBack_S()
-{
-	PrintLetter.push_back('S');
-}
-
-void SetPlayerName::PushBack_T()
-{
-	PrintLetter.push_back('T');
-}
-
-void SetPlayerName::PushBack_U()
-{
-	PrintLetter.push_back('U');
-}
-
-void SetPlayerName::PushBack_V()
-{
-	PrintLetter.push_back('V');
-}
-
-void SetPlayerName::PushBack_W()
-{
-	PrintLetter.push_back('W');
-}
-
-void SetPlayerName::PushBack_X()
-{
-	PrintLetter.push_back('X');
-}
-
-void SetPlayerName::PushBack_Y()
-{
-	PrintLetter.push_back('Y');
-}
-
-void SetPlayerName::PushBack_Z()
-{
-	PrintLetter.push_back('Z');
-}
-
-void SetPlayerName::PushBack_a()
-{
-	PrintLetter.push_back('a');
-}
-
-void SetPlayerName::PushBack_b()
-{
-	PrintLetter.push_back('b');
-}
-
-void SetPlayerName::PushBack_c()
-{
-	PrintLetter.push_back('c');
-}
-
-void SetPlayerName::PushBack_d()
-{
-	PrintLetter.push_back('d');
-}
-
-void SetPlayerName::PushBack_e()
-{
-	PrintLetter.push_back('e');
-}
-
-void SetPlayerName::PushBack_f()
-{
-	PrintLetter.push_back('f');
-}
-
-void SetPlayerName::PushBack_g()
-{
-	PrintLetter.push_back('g');
-}
-
-void SetPlayerName::PushBack_h()
-{
-	PrintLetter.push_back('h');
-}
-
-void SetPlayerName::PushBack_i()
-{
-	PrintLetter.push_back('i');
-}
-
-void SetPlayerName::PushBack_j()
-{
-	PrintLetter.push_back('j');
-}
-
-void SetPlayerName::PushBack_k()
-{
-	PrintLetter.push_back('k');
-}
-
-void SetPlayerName::PushBack_l()
-{
-	PrintLetter.push_back('l');
-}
-
-void SetPlayerName::PushBack_m()
-{
-	PrintLetter.push_back('m');
-}
-
-void SetPlayerName::PushBack_n()
-{
-	PrintLetter.push_back('n');
-}
-
-void SetPlayerName::PushBack_o()
-{
-	PrintLetter.push_back('o');
-}
-
-void SetPlayerName::PushBack_p()
-{
-	PrintLetter.push_back('p');
-}
-
-void SetPlayerName::PushBack_q()
-{
-	PrintLetter.push_back('q');
-}
-
-void SetPlayerName::PushBack_r()
-{
-	PrintLetter.push_back('r');
-}
-
-void SetPlayerName::PushBack_s()
-{
-	PrintLetter.push_back('s');
-}
-
-void SetPlayerName::PushBack_t()
-{
-	PrintLetter.push_back('t');
-}
-
-void SetPlayerName::PushBack_u()
-{
-	PrintLetter.push_back('u');
-}
-
-void SetPlayerName::PushBack_v()
-{
-	PrintLetter.push_back('v');
-}
-
-void SetPlayerName::PushBack_w()
-{
-	PrintLetter.push_back('w');
-}
-
-void SetPlayerName::PushBack_x()
-{
-	PrintLetter.push_back('x');
-}
-
-void SetPlayerName::PushBack_y()
-{
-	PrintLetter.push_back('y');
-}
-
-void SetPlayerName::PushBack_z()
-{
-	PrintLetter.push_back('z');
 }
 
 void SetPlayerName::PopBack_PrintLetter(Button* Button)
--- WinAPI_HSM_PrincessMaker2/GameEngineContents/SetPlayerName.h
+++ WinAPI_HSM_PrincessMaker2/GameEngineContents/SetPlayerName.h
@@ -41,7 +41,7 @@ private:
 	static std::string PrintLetter;
 	GameEngineRender* PrintLetterRender1 = nullptr;
 	GameEngineRender* PrintLetterRender2 = nullptr;
-	GameEngineRender* PrintLetterShadow = nullptr;
+	GameEngineRender* PrintLetterRender3 = nullptr;
 
 	GameEngineRender* ExplainRender1 = nullptr;
 	GameEngineRender* ExplainRender2 = nullptr;
@@ -107,60 +107,6 @@ private:
 	void SetLetterEraseButtonRender();
 
 	static void PushBack_Button(Button* _Btn);
-
-	static void PushBack_A();
-	static void PushBack_B();
-	static void PushBack_C();
-	static void PushBack_D();
-	static void PushBack_E();
-	static void PushBack_F();
-	static void PushBack_G();
-	static void PushBack_H();
-	static void PushBack_I();
-	static void PushBack_J();
-	static void PushBack_K();
-	static void PushBack_L();
-	static void PushBack_M();
-	static void PushBack_N();
-	static void PushBack_O();
-	static void PushBack_P();
-	static void PushBack_Q();
-	static void PushBack_R();
-	static void PushBack_S();
-	static void PushBack_T();
-	static void PushBack_U();
-	static void PushBack_V();
-	static void PushBack_W();
-	static void PushBack_X();
-	static void PushBack_Y();
-	static void PushBack_Z();
-
-	static void PushBack_a();
-	static void PushBack_b();
-	static void PushBack_c();
-	static void PushBack_d();
-	static void PushBack_e();
-	static void PushBack_f();
-	static void PushBack_g();
-	static void PushBack_h();
-	static void PushBack_i();
-	static void PushBack_j();
-	static void PushBack_k();
-	static void PushBack_l();
-	static void PushBack_m();
-	static void PushBack_n();
-	static void PushBack_o();
-	static void PushBack_p();
-	static void PushBack_q();
-	static void PushBack_r();
-	static void PushBack_s();
-	static void PushBack_t();
-	static void PushBack_u();
-	static void PushBack_v();
-	static void PushBack_w();
-	static void PushBack_x();
-	static void PushBack_y();
-	static void PushBack_z();
 
 	static void PopBack_PrintLetter(Button* Button);
 
