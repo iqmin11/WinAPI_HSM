30222a4ebd7ff256910fb2b34557faece8ea2b74
승민 황 <iqmin11@gmail.com>
2023-02-17 21:05:31 +0900

    황승민 : 엔진 업데이트

--- WinAPI_HSM_PrincessMaker2/GameEngineContents/SelectionMenu.cpp
+++ WinAPI_HSM_PrincessMaker2/GameEngineContents/SelectionMenu.cpp
@@ -44,18 +44,18 @@ void SelectionMenu::SetSelectionMenu(int _Count, float4 _Pos, float _xScale, PM2
 void SelectionMenu::SetButtonString(int _RenderOrder, int _ButtonIndex, const std::string_view& _Text)
 {
 	StringRenders[_ButtonIndex] = CreateRender(_RenderOrder);
-	StringRenders[_ButtonIndex]->SetText(_Text, 18, "±¼¸²", TextAlign::Left, RGB(255, 255, 255));
+	StringRenders[_ButtonIndex]->SetText(_Text, 18, "±¼¸²", TextAlign::Left, RGB(255, 255, 255), SelectButtons[_ButtonIndex]->GetScale());
 	float4 SetStringPosition = float4::Left * (SelectButtons[_ButtonIndex]->GetScale().half()) + float4::Up * SelectButtons[_ButtonIndex]->GetScale().half() * static_cast<float>(SelectButtons.size() - 1) + float4::Down * (SelectButtons[_ButtonIndex]->GetScale() * static_cast<float>(_ButtonIndex)) + (float4::Up.half() * static_cast<float>(StringRenders[_ButtonIndex]->GetTextHeight()));
 	StringRenders[_ButtonIndex]->SetPosition(SetStringPosition);
 	
 	int OrderDifference = static_cast<int>(PM2RenderOrder::Menu0_Display) - static_cast<int>(PM2RenderOrder::Menu0_Display_Shadow);
 	StringShadowRenders[_ButtonIndex] = CreateRender(_RenderOrder - OrderDifference);
-	StringShadowRenders[_ButtonIndex]->SetText(_Text, 18, "±¼¸²", TextAlign::Left, RGB(0, 0, 0));
+	StringShadowRenders[_ButtonIndex]->SetText(_Text, 18, "±¼¸²", TextAlign::Left, RGB(0, 0, 0), SelectButtons[_ButtonIndex]->GetScale());
 	StringShadowRenders[_ButtonIndex]->SetPosition(SetStringPosition + float4{ 0,2 });
 
 	OrderDifference = static_cast<int>(PM2RenderOrder::Menu0_Display) - static_cast<int>(PM2RenderOrder::Menu0_Display_Effect);
 	StringHoverRenders[_ButtonIndex] = CreateRender(_RenderOrder - OrderDifference);
-	StringHoverRenders[_ButtonIndex]->SetText(_Text, 18, "±¼¸²", TextAlign::Left, RGB(236, 152, 132));
+	StringHoverRenders[_ButtonIndex]->SetText(_Text, 18, "±¼¸²", TextAlign::Left, RGB(236, 152, 132), SelectButtons[_ButtonIndex]->GetScale());
 	StringHoverRenders[_ButtonIndex]->SetPosition(SetStringPosition + float4{ 0,1 });
 
 	StringHoverRenders[_ButtonIndex]->Off();
--- WinAPI_HSM_PrincessMaker2/GameEngineCore/GameEngineRender.cpp
+++ WinAPI_HSM_PrincessMaker2/GameEngineCore/GameEngineRender.cpp
@@ -90,13 +90,14 @@ void GameEngineRender::FrameAnimation::Render(float _DeltaTime)
 	}
 }
 
-void GameEngineRender::SetText(const std::string_view& _Text, const int _TextHeight, const std::string_view& _TextType, const TextAlign _TextAlign, const COLORREF _TextColor)
+void GameEngineRender::SetText(const std::string_view& _Text, const int _TextHeight, const std::string_view& _TextType, const TextAlign _TextAlign, const COLORREF _TextColor, float4 _TextBoxScale)
 {
 	RenderText = _Text;
 	TextHeight = _TextHeight;
 	TextType = _TextType;
 	Align = _TextAlign;
 	TextColor = _TextColor;
+	TextBoxScale = _TextBoxScale;
 }
 
 void GameEngineRender::Render(float _DeltaTime)
@@ -147,7 +148,16 @@ void GameEngineRender::TextRender(float _DeltaTime)
 	SetTextColor(hdc, TextColor);
 	SetBkMode(hdc, TRANSPARENT);
 
-	TextOutA(GameEngineWindow::GetDoubleBufferImage()->GetImageDC(), RenderPos.ix(), RenderPos.iy(), RenderText.c_str(), static_cast<int>(RenderText.size()));
+	RECT Rect;
+	Rect.left = RenderPos.ix();
+	Rect.top = RenderPos.iy();
+	Rect.right = RenderPos.ix() + TextBoxScale.ix();
+	Rect.bottom = RenderPos.iy() + TextBoxScale.iy();
+
+	DrawTextA(GameEngineWindow::GetDoubleBufferImage()->GetImageDC(), RenderText.c_str(), static_cast<int>(RenderText.size()), &Rect, DT_LEFT);
+
+
+	// TextOutA(GameEngineWindow::GetDoubleBufferImage()->GetImageDC(), RenderPos.ix(), RenderPos.iy(), RenderText.c_str(), static_cast<int>(RenderText.size()));
 
 	SelectObject(hdc, OldFont);
 	DeleteObject(hFont);
